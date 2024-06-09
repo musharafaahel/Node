713959f6d45d67b1fd618957b97ddae8cde35f88
musharafaahel <musaahel@gmail.com>
2024-06-09 12:14:05 +0530

    QR Project Completed

--- 2.2+Native+Modules/2.2 Native Modules/index.js
+++ 2.2+Native+Modules/2.2 Native Modules/index.js
@@ -1,1 +1,16 @@
-const fs = require("")
+const fs = require("fs");
+
+// fs.writeFile("message.txt", "Hello from node.js", (err) => {
+//     if(err) throw err;
+//     console.log("File has be saved.");
+// });
+
+
+
+fs.readFile("message.txt",'utf-8', (err,data) => {
+    if(err) throw err;
+    console.log("File Read");
+    console.log(data);
+});
+
+// console.log(content)
---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,13 @@
+// const generateName = require("sillyname");
+
+// import generateName from "sillyname";
+import {randomSuperhero} from "superheroes";
+
+// let sillyName = generateName();
+
+let superHero = randomSuperhero();
+
+// console.log(`My name is ${sillyName}.`);
+
+
+console.log(`I am ${superHero}!!!`);
